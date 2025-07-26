d720a47c79e828eb4ede50fed99a3669201fdc27
botstech1 <mkuu4634@gmail.com>
2025-07-26 19:50:05 +0300

    .

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "DML-MD~LBshhCLa#i1b2XlzYjo0oNUKeLeYpHcxullvuPsF2qzVmHCKbl3s",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DML-MD 🤍*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY ¥-𝐖𝐈𝐓𝐂𝐇𝐄𝐑-¥ 🤍*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
@@ -31,19 +31,19 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/vcdwmp.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "DML-MD",
+BOT_NAME: process.env.BOT_NAME || "💠 𝐄𝐋𝐈𝐓𝐄 𝐂𝐘𝐁𝐄𝐑𝐕𝐀𝐔𝐋𝐓 💠",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "DML-MD",
+STICKER_NAME: process.env.STICKER_NAME || "💠 𝐄𝐋𝐈𝐓𝐄 𝐂𝐘𝐁𝐄𝐑𝐕𝐀𝐔𝐋𝐓 💠",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "255713XXXX",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254762334605",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "DML-TECH",
+OWNER_NAME: process.env.OWNER_NAME || "¥_𝐖𝐈𝐓𝐂𝐇𝐄𝐑_¥",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© POWERED BY DML-TECH*",
 // add bot owner name    
@@ -57,9 +57,9 @@ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 // make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "true",
 // make anti link true,false for groups 
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
@@ -67,13 +67,13 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255622220680",
+DEV: process.env.DEV || "254762334605",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
